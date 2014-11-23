23c8f02237d543c1f2835c92999e9c98a5dabe97
rmaratos <rmaratos@andrew.cmu.edu>
2014-11-23 14:26:55 -0500

    initial arrow drawing

--- js/game.js
+++ js/game.js
@@ -12,26 +12,25 @@ define(function(require) {
     'use strict';
 
     var
-    Fish = require('fish'),
+    Note = require('note'),
     C    = require('constants'),
     Util = require('util');
 
     var Game = {
         start: function() {
-            this.player = new Fish(view.bounds.center);
-            this.lastFish = 0;
+            this.player = [];
+            // this.lastFish = 0;
             this.score = 0;
             this.started = true;
 
-            for (var i = 0; i < 10; i++) {
-                this.newEnemy();
-            }
+            // for (var i = 0; i < 10; i++) {
+            //     this.newEnemy();
+            // }
         },
 
         end: function() {
             document.getElementById('startBox').style.display = 'block';
             // document.getElementById('score').innerHTML = 'Your score was ' + this.score;
-            project.activeLayer.removeChildren();
             this.started = false;
         },
 
@@ -41,91 +40,108 @@ define(function(require) {
                 return;
             }
 
-            var player = this.player;
-
+            // var player = this.player;
+            var arrows = [];
+            for (var i=0; i < this.player.length; i++) {
+                console.log("removing");
+                console.log(this.player[i]);
+                this.player[i].remove();
+                console.log(this.player[i]);
+            }
             // handle keyboard events for moving fish
-            if (Key.isDown('w') || Key.isDown('up')) {
-                player.addVelocity([0, -C.ACCELERATION]);
-            } else if (Key.isDown('s') || Key.isDown('down')) {
-                player.addVelocity([0, C.ACCELERATION]);
+            if (Key.isDown('w') || Key.isDown("up")) {
+                arrows.push("up");
+            }
+            if (Key.isDown('s') || Key.isDown('down')) {
+                arrows.push("down");
             }
 
-            if (Key.isDown('a') || Key.isDown('left')) {
-                player.addVelocity([-C.ACCELERATION, 0]);
-            } else if (Key.isDown('d') || Key.isDown('right')) {
-                player.addVelocity([C.ACCELERATION, 0]);
+            if (Key.isDown('a') || Key.isDown("left")) {
+                arrows.push("left");
             }
 
-            // do simple 2D physics for the player
-            // calculate velocity with deceleration
-            var playerBounds = player.strokeBounds;
+            if (Key.isDown('d') || Key.isDown("right")) {
+                arrows.push("right");
+            }
+            this.player = [];
 
-            if (!view.bounds.contains(playerBounds)) {
-
-                // fixme: player can still get stuck. detection isn't
-                var nx = playerBounds.width / 2, ny = playerBounds.height / 2;
-                if ((player.position.x <= nx && player.velocity[0] < 0) ||
-                    (player.position.x >= view.bounds.width - nx && player.velocity[0] > 0)) {
-                    player.velocity[0] = 0;
-                } else if((player.position.y <= ny && player.velocity[1] < 0) ||
-                          (player.position.y >= view.bounds.height - ny && player.velocity[1] > 0)) {
-                    player.velocity[1] = 0;
-                }
+            for (var i=0; i < arrows.length; i++)
+            {
+                this.player.push(new Note(arrows[i]));
             }
 
+            console.log(arrows);
 
-            // move the fish by the given velocity
-            player.position = player.position.add(player.velocity);
+            // // do simple 2D physics for the player
+            // // calculate velocity with deceleration
+            // var playerBounds = player.strokeBounds;
 
-            // change the fish's orientation accordingly
-            if ((player.velocity[0] > 0 && player.orientation == C.LEFT) ||
-                (player.velocity[0] < 0 && player.orientation == C.RIGHT)) {
-                player.rotate(180);
-                player.orientation = !player.orientation;
-            }
+            // if (!view.bounds.contains(playerBounds)) {
 
-            // handle enemy fish logic and collisions
-            _.forEach(project.activeLayer.children, function(otherFish) {
-                if (player.id === otherFish.id) {
-                    return;
-                }
+            //     // fixme: player can still get stuck. detection isn't
+            //     var nx = playerBounds.width / 2, ny = playerBounds.height / 2;
+            //     if ((player.position.x <= nx && player.velocity[0] < 0) ||
+            //         (player.position.x >= view.bounds.width - nx && player.velocity[0] > 0)) {
+            //         player.velocity[0] = 0;
+            //     } else if((player.position.y <= ny && player.velocity[1] < 0) ||
+            //               (player.position.y >= view.bounds.height - ny && player.velocity[1] > 0)) {
+            //         player.velocity[1] = 0;
+            //     }
+            // }
 
-                var
-                otherBounds = otherFish.strokeBounds,
-                overlap = otherBounds.intersect(playerBounds),
-                overlapArea = overlap.width * overlap.height,
-                otherArea   = otherBounds.width * otherBounds.height;
 
-                if (overlapArea / otherArea > C.MIN_EAT_OVERLAP & overlap.width > 0) {
-                    if (playerBounds.width > otherBounds.width) {
-                        player.scale((playerBounds.width + C.SIZE_GAIN * (otherBounds.width / playerBounds.width + 0.3)) / playerBounds.width);
-                        otherFish.remove();
-                        this.score++;
+            // // move the fish by the given velocity
+            // player.position = player.position.add(player.velocity);
 
-                        player.setColor(otherFish.getColor());
-                    } else {
-                        this.end();
-                    }
-                }
+            // // change the fish's orientation accordingly
+            // if ((player.velocity[0] > 0 && player.orientation == C.LEFT) ||
+            //     (player.velocity[0] < 0 && player.orientation == C.RIGHT)) {
+            //     player.rotate(180);
+            //     player.orientation = !player.orientation;
+            // }
 
-                otherFish.position = otherFish.position.add(otherFish.velocity);
+            // // handle enemy fish logic and collisions
+            // _.forEach(project.activeLayer.children, function(otherFish) {
+            //     if (player.id === otherFish.id) {
+            //         return;
+            //     }
 
-                // todo: add GC
-                /*if (!other_bounds.intersects(view.bounds) && !view.bounds.contains(other_bounds)) {
-                  otherFish.remove();
-                  }*/
-            }, this);
+            //     var
+            //     otherBounds = otherFish.strokeBounds,
+            //     overlap = otherBounds.intersect(playerBounds),
+            //     overlapArea = overlap.width * overlap.height,
+            //     otherArea   = otherBounds.width * otherBounds.height;
 
-           Util.decelerate(player.velocity);
+            //     if (overlapArea / otherArea > C.MIN_EAT_OVERLAP & overlap.width > 0) {
+            //         if (playerBounds.width > otherBounds.width) {
+            //             player.scale((playerBounds.width + C.SIZE_GAIN * (otherBounds.width / playerBounds.width + 0.3)) / playerBounds.width);
+            //             otherFish.remove();
+            //             this.score++;
 
-            // generate fishes every second
-            if (e.time - this.lastFish >= C.FISH_SPAWN_TIME) {
-                this.newEnemy();
-                this.lastFish = e.time;
-            }
+            //             player.setColor(otherFish.getColor());
+            //         } else {
+            //             this.end();
+            //         }
+            //     }
+
+            //     otherFish.position = otherFish.position.add(otherFish.velocity);
+
+            //     // todo: add GC
+            //     /*if (!other_bounds.intersects(view.bounds) && !view.bounds.contains(other_bounds)) {
+            //       otherFish.remove();
+            //       }*/
+            // }, this);
+
+           // Util.decelerate(player.velocity);
+
+           //  // generate fishes every second
+           //  if (e.time - this.lastFish >= C.FISH_SPAWN_TIME) {
+           //      this.newEnemy();
+           //      this.lastFish = e.time;
+           //  }
         },
 
-        newEnemy: function() {
+        newNote: function() {
             var pos = Math.random() * view.bounds.height;
             var side = Math.random() > 0.5;
             var enemy = new Fish([side ? view.bounds.width : 0, pos]);
