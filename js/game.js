740203eb3f601e551bcf14d20d38b85d09bb0ccd
rmaratos <rmaratos@andrew.cmu.edu>
2014-11-29 16:13:29 -0500

    It's a game

--- js/game.js
+++ js/game.js
@@ -16,6 +16,7 @@ define(function(require) {
     C    = require('constants'),
     Util = require('util');
     var dirs = ["up", "down", "left", "right"];
+    var pressed = {"up":false, "down":false, "left":false, "right":false};
     var playerNotes = new Group();
     var upNotes = new Group();
     var downNotes = new Group();
@@ -43,6 +44,8 @@ define(function(require) {
             }
 
             this.score = 0;
+            this.textBox = new PointText(new Point(100,100));
+            this.textBox.content = "Score: " + this.score;
             this.started = true;
             this.addingNote = false;
 
@@ -50,13 +53,15 @@ define(function(require) {
 
         end: function() {
             document.getElementById('startBox').style.display = 'block';
-            // document.getElementById('score').innerHTML = 'Your score was ' + this.score;
+            document.getElementById('score').innerHTML = 'Your score was ' + this.score;
             this.started = false;
 
             playerNotes.removeChildren();
         },
 
         loop: function(e) {
+            // console.log(pressed);
+            this.textBox.content = "Score: " + this.score;
 
             if (!this.started) {
                 return;
@@ -66,18 +71,26 @@ define(function(require) {
             if (Key.isDown('w') || Key.isDown("up")) {
                 arrows.push("up");
             }
+            else
+                pressed["up"] = false;
 
             if (Key.isDown('s') || Key.isDown('down')) {
                 arrows.push("down");
             }
+            else
+                pressed["down"] = false;
 
             if (Key.isDown('a') || Key.isDown("left")) {
                 arrows.push("left");
             }
+            else
+                pressed["left"] = false;
 
             if (Key.isDown('d') || Key.isDown("right")) {
                 arrows.push("right");
             }
+            else
+                pressed["right"] = false;
 
             if (Key.isDown('n')) {
                 if (!this.addingNote) {
@@ -90,24 +103,26 @@ define(function(require) {
 
             _.forEach(playerNotes.children, function(note) {
                 var enable = arrows.indexOf(note.name)>=0;
+                if (enable)
+                    this.pressed(note.name);
                 note.img.visible = enable;
 
             }, this);
 
             _.forEach(upNotes.children, function(note) {
-                note.move();
+                this.moveNote(note);
             }, this);
 
             _.forEach(downNotes.children, function(note) {
-                note.move();
+                this.moveNote(note);
             }, this);
 
             _.forEach(leftNotes.children, function(note) {
-                note.move();
+                this.moveNote(note);
             }, this);
 
             _.forEach(rightNotes.children, function(note) {
-                note.move();
+                this.moveNote(note);
             }, this);
 
 
@@ -121,8 +136,46 @@ define(function(require) {
             }
 
         },
+        moveNote: function(note) {
+            note.move();
+            if (note.img.position.y < -25)
+            {
+                note.img.remove();
+                note.remove();
+                console.log(":(");
+                this.score -= 10;
+            }
+        },
+        pressed: function(dir) {
+            if (pressed[dir])
+                return;
+            console.log(dir);
+            pressed[dir] = true;
+            var notes;
+            if (dir == 'left')
+                notes = leftNotes;
+            else if (dir == 'down')
+                notes = downNotes;
+            else if (dir == 'up')
+                notes = upNotes;
+            else if (dir == 'right')
+                notes = rightNotes;
+
+            _.forEach(notes.children, function(note) {
+                console.log(note.img.position.y);
+                var y = note.img.position.y;
+                if ((25 < y) && (y < 75)){
+                    this.score += 25;
+                    console.log("bang!");
+                    note.img.remove();
+                    note.remove();
+                }
+                else
+                    this.score -= 5;
+            }, this);
 
 
+        },
 
         // Add new falling note to screen
         newFallingNote: function() {
